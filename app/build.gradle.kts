7f48134319561ace573880ef42c7dc6c0e374a7a
isldyq <isldyq@outlook.com>
2024-07-26 16:24:43 +0800

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.example.androidunittest"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.androidunittest"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation ("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        maven(url="https://maven.aliyun.com/nexus/content/groups/public/")
+        maven(url="https://maven.aliyun.com/nexus/content/repositories/jcenter")
+        maven(url="https://maven.aliyun.com/repository/google")
+        maven(url="https://maven.aliyun.com/repository/gradle-plugin")
+        maven(url = "https://jitpack.io")
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven(url="https://maven.aliyun.com/nexus/content/groups/public/")
+        maven(url="https://maven.aliyun.com/nexus/content/repositories/jcenter")
+        maven(url="https://maven.aliyun.com/repository/google")
+        maven(url="https://maven.aliyun.com/repository/gradle-plugin")
+        maven(url = "https://jitpack.io")
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "AndroidUnitTest"
+include(":app")
+ 
